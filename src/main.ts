2fd12df53285220ab92f9a23ae1018eb3b678157
Salvador <perezg.josesal@gmail.com>
2022-03-02 13:27:11 -0600

    Feat - Se agrega plugin de Quasar al proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/eva-icons/eva-icons.css'
+import '@quasar/extras/bootstrap-icons/bootstrap-icons.css'
+import 'quasar/src/css/index.sass'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+myApp.mount('#app')
