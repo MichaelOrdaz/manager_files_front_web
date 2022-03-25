445f121429210eac8e2fcd48c7d7a015fdd5308a
Salvador <perezg.josesal@gmail.com>
2022-03-25 12:58:41 -0600

    Feat - Se agrega alias en configuración para el proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import {fileURLToPath, URL} from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
     plugins: [
         vue({
             template: { transformAssetUrls }
