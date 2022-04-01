61179b0ba5bb67780efb68f9cbd156ecf6b388c2
Salvador <perezg.josesal@gmail.com>
2022-04-01 12:42:41 -0600

    Feat - Se integra por completo PModal.vue

--- src/components/componentsRegister.ts
+++ src/components/componentsRegister.ts
@@ -6,6 +6,7 @@ import PInput from './Molecules/PInput.vue'
 import PSelect from './Molecules/PSelect.vue'
 import PLetterIcon from './Atoms/PLetterIcon.vue'
 import PMiniCard from './Molecules/MiniCard.vue'
+import PModal from './Molecules/PModal.vue'
 
 export default function componentsRegister (app:App): void {
     app.component('PButton', PButton)
@@ -15,4 +16,5 @@ export default function componentsRegister (app:App): void {
     app.component('PSelect', PSelect)
     app.component('PLetterIcon',PLetterIcon)
     app.component('PMiniCard', PMiniCard)
+    app.component('PModal', PModal)
 }
