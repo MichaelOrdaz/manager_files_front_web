1b02a96c2f8df1b602075a761bbb306e588672a9
Salvador <perezg.josesal@gmail.com>
2022-04-11 11:54:00 -0500

    Feat - Se agrega tabla de gestión de usuarios

---
+++ src/Types/User.ts
@@ -0,0 +1,14 @@
+export type User = {
+    id: number,
+    email?: string,
+    name: string,
+    lastName?: string,
+    secondLastName?: string,
+    phone?: string,
+    image?: string | File,
+    password?: string,
+    departament_id?: number,
+    updated_at?: string,
+    created_at?: string,
+    deleted_at?: string,
+}
