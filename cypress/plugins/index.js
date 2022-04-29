3d263471666c36a2c31d9ba4322e455da1db23b7
Salvador <perezg.josesal@gmail.com>
2022-04-29 13:36:55 -0500

    Se modifica image de gitlab-cy para deploy y se agregan paquetes

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,17 +15,4 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-const path = require('path')
-const { startDevServer } = require('@cypress/vite-dev-server')
 
-module.exports = (on, config) => {
-    on('dev-server:start', (options) => {
-        return startDevServer({
-            options,
-            viteConfig: {
-                configFile: path.resolve(__dirname, '..', '..', 'vite.config.js'),
-            },
-        })
-    })
-}
